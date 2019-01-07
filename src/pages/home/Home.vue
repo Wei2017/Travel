f8207239ea2ddc69dbea3614a1a210133f53d308
XiaoWeiGe <527188405@qq.com>
2019-01-07 17:28:02 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 /* 引用组件 */
-import HomeHeader from './components/HomeHeader'
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default{
   name: 'Home',
   /* 注册局部组件 */
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop:true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/d6/daa880b254940402.jpg_750x200_b114308a.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // >>> 样式穿透 不受scoped的影响
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color:#fff
+  .wrapper
+    overflow:hidden
+    width:100%
+    height:0
+    padding-bottom:31.25%
+    .swiper-img
+      width:100%
+</style>
