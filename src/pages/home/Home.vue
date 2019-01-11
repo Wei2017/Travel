2c7e36e7f9212bf457cb0b282717dadc1acc3d11
XiaoWeiGe <527188405@qq.com>
2019-01-11 15:08:33 +0800

    update config

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -28,7 +28,7 @@ export default{
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/static/mock/index.json')
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
